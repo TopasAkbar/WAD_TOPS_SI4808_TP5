a363a64edcf156b738570009a7b56c969fc63ddc
Man Rezki Setiawan <manrezki2@gmail.com>
2025-12-01 16:31:24 +0700

    TP MODUL 5

---
+++ TPMODUL5_MAN/app/Http/Controllers/BooksController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Book;
+use App\Http\Resources\BookResource;
+
+class BooksController extends Controller
+{
+    /**
+     * ==========1===========
+     * Tampilkan daftar semua buku
+     */
+    public function index()
+    {
+        $books = book::all();
+        return bookresource::collection($books);
+
+    }
+
+    /**
+     * ==========2===========
+     * Simpan buku baru ke dalam penyimpanan.
+     */
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+        'title'           => 'required|string|max:255',
+        'author'          => 'required|string|max:255',
+        'published_year'  => 'required|integer|min:1000|max:9999',
+         ]);
+
+         if ($validator->fails()) {
+        return response()->json([
+            'message' => 'Please check your request',
+            'errors'  => $validator->errors()
+        ], 422);
+        }
+
+        $book = Book::create($validator->validated());
+
+
+       return (new BookResource($book))
+                ->additional(['message' => 'Item created successfully'])
+                ->response()
+                ->setStatusCode(201);
+
+
+    }
+
+    /**
+     * =========3===========
+     * Tampilkan detail buku tertentu.
+     */
+    public function show(string $id)
+    {
+        $book = Book::find($id);
+
+        if (!$book) {
+        return response()->json(['message' => 'Book not found'], 404);
+        }
+
+        return new BookResource($book);
+    }
+
+    /**
+     * =========4===========
+     * Fungsi untuk memperbarui data buku tertentu
+     */
+    public function update(Request $request, string $id)
+    {
+        $validator = Validator::make($request->all(), [
+        'title' => 'sometimes|required|string|max:255',
+        'author' => 'sometimes|required|string|max:255',
+        'published_year' => 'sometimes|required|integer|digits:4|min:1900',
+        'is_available' => 'sometimes|required|boolean',
+        ]);
+
+         $book = Book::find($id);
+
+        if (!$book) {
+            return response()->json(['message' => 'Book not found'], 404);
+        }
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Please check your request',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $book->update($validator->validated());
+        return (new BookResource($book))
+            ->additional(['message' => 'Book updated successfully'])
+            ->response()
+            ->setStatusCode(200);
+    }
+
+    /**
+     * =========5===========
+     * Hapus buku tertentu dari penyimpanan.
+     */
+    public function destroy(string $id)
+    {
+        $book = Book::find($id);
+        if (!$book) {
+            return response()->json(['message' => 'Book not found'], 404);
+        }
+
+        $book->delete();
+        return response()->json(['message' => 'Book deleted successfully'], 200);
+    }
+
+    /**
+     * =========6===========
+     * Ubah status ketersediaan buku (ubah field is_available)
+     */
+    public function borrowReturn(string $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->is_available = $book->is_available == 1 ? 0 : 1;
+        $book->save();
+        $message = $book->is_available ? 'Book returned (available)' : 'Book borrowed (not available)';
+        return response()->json([
+            'message' => $message,
+            'book' => $book
+        ]);
+
+    }
+}
